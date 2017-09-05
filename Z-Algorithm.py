2d26ef26c67bb4337fc9ae161c6ff7741d9f2055
Carsen Ball <ballcarsen@gmail.com>
2017-09-05 14:51:07 -0600

    comment edits

--- Z-Algorithm.py
+++ Z-Algorithm.py
@@ -11,4 +11,5 @@ def main():
     #Note: Prof. Mumey would like us to write the z-alg and
     #then create a seperate file with the pattern implementation
     #For the pattern implementation we only have to keep track of
-    #the z values for the indicies coresponding to the pattern
+    #the z values for the indicies coresponding to the pattern so
+    #the code will just be a hair different. 
