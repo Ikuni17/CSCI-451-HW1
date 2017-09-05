7a1e9bcebc543fb8fd522125ea724d1f82551e10
Carsen Ball <ballcarsen@gmail.com>
2017-09-05 14:45:25 -0600

    checking compatability with pydev

--- Z-Algorithm.py
+++ Z-Algorithm.py
@@ -8,3 +8,7 @@ September 12, 2017
 def main():
     pass
     # TODO: Implement the Z-Algorithm with pattern matching
+    #Note: Prof. Mumey would like us to write the z-alg and
+    #then create a seperate file with the pattern implementation
+    #For the pattern implementation we only have to keep track of
+    #the z values for the indicies coresponding to the pattern
