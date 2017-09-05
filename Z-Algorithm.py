8f33309b405f2c8f153f3d1a009e68bdd0e01da8
Brad White <white.brad17@gmail.com>
2017-09-05 13:46:00 -0600

    Initial commit

---
+++ Z-Algorithm.py
@@ -0,0 +1,10 @@
+'''
+Chase Austvold, Carsen Ball, Bradley White
+Homework #1: Z-Algorithm and Pattern Matching
+CSCI 451/551
+September 12, 2017
+'''
+
+def main():
+    pass
+    # TODO: Implement the Z-Algorithm
