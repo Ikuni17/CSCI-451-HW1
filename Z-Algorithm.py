c1dcceeec5a434568408d700bf4941f181aa0f20
BradWhite <white.brad17@gmail.com>
2017-09-07 08:32:29 -0600

    Formatting

--- Z-Algorithm.py
+++ Z-Algorithm.py
@@ -4,10 +4,12 @@ Homework #1: Z-Algorithm and Pattern Matching
 CSCI 451/551
 September 12, 2017
 '''
-#TODO: Implement the Z-Algorithm with pattern matching(separate file)
+
+
+# TODO: Implement the Z-Algorithm with pattern matching(separate file)
 class ZAlgorithm():
-    #creates global variables l,r,k and an array to hold the z scores
-    #creates an array from the input string
+    # creates global variables l,r,k and an array to hold the z scores
+    # creates an array from the input string
     def __init__(self, input_text):
         self.l = 0
         self.r = 0
@@ -15,42 +17,42 @@ class ZAlgorithm():
         self.z_scores = []
         self.input_string = list(input_text)
 
-    
-    #Will decide what case to use based on l,k,r 
+    # Will decide what case to use based on l,k,r
     def driver(self):
-        #if statement to decide what case to chose
+        # if statement to decide what case to chose
         if self.k > self.r:
             run_z.case_1()
-        else :
+        else:
             run_z.case_2()
-    
-    #case 1, k > r    
+
+    # case 1, k > r
     def case_1(self):
         count = 0
         char_k = self.input_string[self.k]
 
-        #compares the char at k + count with the char at count
-        while ord(char_k) == ord(self.input_string[count]) :
+        # compares the char at k + count with the char at count
+        while ord(char_k) == ord(self.input_string[count]):
             count += 1
             char_k = self.input_string(self.k + count)
 
-        #update values
+        # update values
         self.z_scores[self.k] = count
         self.l = self.k
         self.r = self.k + count
         run_z.driver()
 
-    #case 2a, K <= R && Z_k < length of Beta
-    #case 2b, k <= r && Z_k >= length of Beta
-    #Beta is the string from [k,r]
+    # case 2a, K <= R && Z_k < length of Beta
+    # case 2b, k <= r && Z_k >= length of Beta
+    # Beta is the string from [k,r]
     def case_2(self):
-        beta = self.input_string[self.k,self.r + 1]
-        if(self.z_scores[self.k] < len(beta)) :
-            #case 2a
+        beta = self.input_string[self.k, self.r + 1]
+        if (self.z_scores[self.k] < len(beta)):
+            # case 2a
             pass
         else:
-            #case 2b
+            # case 2b
             pass
+
 
 if __name__ == "__main__":
     input_text = input("Enter your desired string: ")
