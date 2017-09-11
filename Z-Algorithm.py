4ea1fc0e021a5adcb409677b3ad0ab5d6564fe7c
Carsen Ball <ballcarsen@gmail.com>
2017-09-11 16:43:35 -0600

    pattern matching

--- Z-Algorithm.py
+++ Z-Algorithm.py
@@ -9,14 +9,19 @@ import timeit
 from random import choice
 from string import ascii_lowercase
 
+#Accepts a string, and returns a list of the z scores
 def Z_algorithm(s):
+    #will hold the left and right side of the "box"
     l = 0
     r = 0
+    #Will hold the z scores for all the indicies in the string
     z_scores = [0] * len(s)
+    #Creates a list from the pattern
     s_list = list(s)
 
+    #Increments by each index in the string, calculating the z score at that index
     for k in range(1, len(s)):
-        # Case 1
+        # Case 1, Brute force prefix recognition
         if k > r:
             count = 0
             while (k + count) < len(s_list) and s_list[k + count] == s_list[count]:
@@ -25,7 +30,7 @@ def Z_algorithm(s):
             if count > 0:
                 l = k
                 r = k + count - 1
-        # Case 2
+        # Case 2, if we are within a "box"
         else:
             beta = s_list[k:(r + 1)]
             # Case 2a
@@ -40,6 +45,7 @@ def Z_algorithm(s):
                 r = count - 1
                 z_scores[k] = count - k
 
+    #Returns the Z scores at each index in the string
     return z_scores
 
 def generate_string(length):
@@ -47,18 +53,13 @@ def generate_string(length):
     return ''.join(choice('abc') for i in range(length))
 
 if __name__ == "__main__":
-    '''input_text = input("Enter your desired string: ")
-    run_z = ZAlgorithm(input_text)
-    run_z.driver()
-    run_z.print_z_scores()'''
-
-    # Correct output should be [0,0,5,0,3,0,1,1,1]
-    #print(Z_algorithm('abababaaa'))
-    timeit.timeit("Z_algorithm('abababaaa')",globals=globals(), number=1)
-
+    #Creates a random strings of length 2^i
     for i in range(2,50):
         length = 2**i
         s_input = generate_string(length)
+        #Prints the iteration of the loop with the coresponding random string length
         print("Starting iteration {0} with string length {1}".format(i-1, length))
+        #Calculates the time it takes to run the Z_algorithm
         time = timeit.timeit("Z_algorithm('{0}')".format(s_input),globals=globals(), number=1)
+        #Prints the time taken to run the z_algorithm on the random string
         print("\tTime taken: {0}".format(time))
