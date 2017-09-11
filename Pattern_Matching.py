86844074c569b7900abb9de7982ad56720ee65ba
BradWhite <white.brad17@gmail.com>
2017-09-11 16:32:54 -0600

    Pattern matching output updated

--- Pattern_Matching.py
+++ Pattern_Matching.py
@@ -11,6 +11,7 @@ from string import ascii_lowercase
 
 length = 0
 
+
 def Z_algorithm(s):
     global length
     l = 0
@@ -44,34 +45,41 @@ def Z_algorithm(s):
                 r = count - 1
                 z_scores[k] = count - k
         if z_scores[k] == length:
-            pattern_locations.append(k - (length +1 ))
+            pattern_locations.append(k - (length + 1))
 
     return pattern_locations
 
-def Pattern_Matching():
-    p = 'aba'
-    s = 'abacabacabasfkdflksdjaba'
+
+def pattern_matching(p, s):
     global length
+    # p = 'aba'
+    # s = 'abacabacabasfkdflksdjaba'
     length = len(p)
-    print('The pattern exists at the indices {0}'.format(Z_algorithm(p + '$' + s)))
+    patterns = Z_algorithm(p + '$' + s)
+    #print('\tThe pattern exists at the indices {0}'.format(patterns))
+    print('\tThe pattern exists {0} times'.format(len(patterns)))
 
 
 def generate_string(length):
-    #return ''.join(choice(ascii_lowercase) for i in range(length))
-    return ''.join(choice('a') for i in range(length))
+    # return ''.join(choice(ascii_lowercase) for i in range(length))
+    return ''.join(choice('ac') for i in range(length))
 
+
+def generate_pattern(length):
+    return ''.join(choice('ac') for i in range(length))
 
 
 if __name__ == "__main__":
 
     # Correct output should be [0,0,5,0,3,0,1,1,1]
-    #print(Z_algorithm('abababaaa'))
-    '''timeit.timeit("Z_algorithm('abababaaa')",globals=globals(), number=1)
+    # print(Z_algorithm('abababaaa'))
+    # timeit.timeit("Z_algorithm('abababaaa')",globals=globals(), number=1)
 
-    for i in range(2,3):
-        length = 2**i
+    for i in range(2, 50):
+        length = 2 ** i
         s_input = generate_string(length)
-        print("Starting iteration {0} with string length {1}".format(i-1, length))
-        time = timeit.timeit("Z_algorithm('{0}')".format(s_input),globals=globals(), number=1)
-        print("\tTime taken: {0}".format(time))'''
-    Pattern_Matching()
+        p_input = generate_pattern(i)
+        print("Starting iteration {0} with string length {1} and pattern length {2}".format(i - 1, length, len(p_input)))
+        time = timeit.timeit("pattern_matching('{0}', '{1}')".format(p_input, s_input), globals=globals(), number=1)
+        print("\tTime taken: {0} seconds".format(time))
+        # pattern_matching()
--- Z-Algorithm.py
+++ Z-Algorithm.py
@@ -44,75 +44,7 @@ def Z_algorithm(s):
 
 def generate_string(length):
     #return ''.join(choice(ascii_lowercase) for i in range(length))
-    return ''.join(choice('a') for i in range(length))
-
-'''class ZAlgorithm():
-    # creates local variables l,r,k and an array to hold the z scores
-    # creates an array from the input string
-    def __init__(self, input_text):
-        self.l = 0
-        self.r = 0
-        self.k = 1
-        self.z_scores = [0] * len(input_text)
-        self.input_string = list(input_text)
-
-    # Will decide what case to use based on l,k,r
-    def driver(self):
-        # if statement to decide what case to chose
-        if self.k < len(self.input_string):
-            if self.k > self.r:
-                self.case_1()
-            else:
-                self.case_2()
-
-    # case 1, k > r
-    def case_1(self):
-        count = 0
-        char_k = self.input_string[self.k]
-
-        # compares the char at k + count with the char at count
-        while ord(char_k) == ord(self.input_string[count]):
-            if self.k + count < (len(self.input_string) - 1):
-                count += 1
-                char_k = self.input_string[self.k + count]
-            else:
-                break
-
-        # update values
-        self.z_scores[self.k] = count
-        self.l = self.k
-        self.r = self.k + count
-        self.driver()
-
-    # case 2a, K <= R && Z_k < length of Beta
-    # case 2b, k <= r && Z_k >= length of Beta
-    # Beta is the string from [k,r]
-    def case_2(self):
-        beta = self.input_string[self.k:self.r + 1]
-        if self.k < (len(self.input_string) - 1):
-            if (self.z_scores[self.k] < len(beta)):
-                # case 2a
-                self.k += 1
-                self.z_scores[self.k] = self.z_scores[self.k - self.l]
-                self.driver()
-            else:
-                # case 2b
-                count = 0
-                char_k = self.input_string[self.r + 1]
-                while ord(char_k) == ord(self.input_string[self.r - self.k + 1 + count]):
-                    if self.k + count < (len(self.input_string) - 1):
-                        count += 1
-                        char_k = self.input_string[self.r + count + 1]
-                    else:
-                        break
-                self.l = self.k
-                self.r = self.r + count
-                self.k += 1
-                self.z_scores[self.k] = self.z_scores[self.r - self.k + 1] + count
-                self.driver()
-
-    def print_z_scores(self):
-        print(self.z_scores)'''
+    return ''.join(choice('abc') for i in range(length))
 
 if __name__ == "__main__":
     '''input_text = input("Enter your desired string: ")
@@ -124,7 +56,7 @@ if __name__ == "__main__":
     #print(Z_algorithm('abababaaa'))
     timeit.timeit("Z_algorithm('abababaaa')",globals=globals(), number=1)
 
-    for i in range(2,3):
+    for i in range(2,50):
         length = 2**i
         s_input = generate_string(length)
         print("Starting iteration {0} with string length {1}".format(i-1, length))
